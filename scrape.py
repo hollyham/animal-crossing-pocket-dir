8a8a284429fa9d7be7b721e3631243bdd98e2a50
Holly Ham <hham2017@gmail.com>
2017-12-28 03:50:00 -0800

    Retrieves names of villagers (no tags)

--- scrape.py
+++ scrape.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://dataquestio.github.io/web-scraping-pages/simple.html' 
+url = 'http://animalcrossing.wikia.com/wiki/Villager_list_(Pocket_Camp)'
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-html = list(soup.children)[2] # Selects contents of html tag
-body = list(html.children)[3] # Selects body of html
-p = list(body.children)[1] # Selects contents of p tag
-print(p.get_text()) # Isolates text inside tag
-
+web_table = soup.find('table', class_='roundy sortable')
+villagers = web_table.find_all('tr')
+for animal in villagers[1:]:# Skips 0 index (table header)
+    try:
+        current = animal.find('a')
+        print(current.get_text())
+        count += 1
+    except AttributeError:
+        print("AttributeError")
