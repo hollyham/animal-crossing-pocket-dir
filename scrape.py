e29fb69d0c7a9fdf8fa78ccf1c17b261bbda45ad
Holly Ham <hham2017@gmail.com>
2017-12-29 03:19:11 -0800

    Scrapes urls of images

--- scrape.py
+++ scrape.py
@@ -8,9 +8,11 @@ soup = BeautifulSoup(page.content, 'html.parser')
 web_table = soup.find('table', class_='roundy sortable')
 villagers = web_table.find_all('tr')
 for animal in villagers[1:]:# Skips 0 index (table header)
+    data = animal.find_all('td')
     try:
-        current = animal.find('a')
+        current = data[0].find('a')
         print(current.get_text())
-        count += 1
+        image = data[1].find('a', href=True)
+        print(image['href'])
     except AttributeError:
-        print("AttributeError")
+        print("Name not found")
