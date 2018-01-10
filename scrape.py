db00b2e2559d9d0948a94ca0d0fd20def4aab5c6
Holly Ham <hham2017@gmail.com>
2018-01-10 02:01:24 -0800

    Writes data to csv file

--- scrape.py
+++ scrape.py
@@ -9,36 +9,36 @@ soup = BeautifulSoup(page.content, 'html.parser')
 web_table = soup.find('table', class_='roundy sortable') # Finds villager table
 villagers = web_table.find_all('tr') # Gets rows of the table
 
+full_data=[] # Villagers with data
 villager_data=[] # Data for each villager 
 for animal in villagers[1:]:# Skips 0 index (table header)
     data = animal.find_all('td')
+
     try:
-        name = data[0].find('a') # Name of villager (name is link to villager's wiki)
-        print(name.get_text())
-        image = data[1].find('a', href=True) 
-        print(image['href'])
+        name = data[0].find('a').get_text() # Name of villager (name is link to villager's wiki)
+        image = data[1].find('a', href=True)['href']
         personality = data[2].find('a').get_text()
-        print(personality)
         species = data[3].find('a').get_text()
-        print(species)
         birthday = data[4].get_text()
-        print(birthday)
         catchphrase = data[5].get_text()
-        print(catchphrase)
         theme = data[6].get_text()
-        print(theme)
+
+        villager_data.extend([name,image,personality,species,birthday,catchphrase,theme])
+
         resources_list = []
         resources = data[7].find_all('a')
+
         for resource in resources:
             resources_list.append(resource.get_text())
-            print(resource.get_text())
-        
+         
+        resources_str = ", ".join(resources_list)
+        villager_data.append(resources_str)
 
-#villager_data=[name,image.get('href')]
     except AttributeError:
         print("Name not found")
-
-#with open("./villagers.csv", "wb") as outfile:
-#    writer= csv.writer(outfile)
-#    for row in data_list:
-#        writer.writerow(row)
+    full_data.append(villager_data[:])
+    del villager_data[:] #equivalent to villager_data.clear()
+with open("./villagers.csv", "w") as outfile:
+    writer= csv.writer(outfile)
+    for row in full_data:
+        writer.writerow(row)
